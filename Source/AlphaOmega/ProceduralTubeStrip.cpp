eaafa34c5c35c9043e17242d30fd01e17888aa25
Toni Lopez <alopezs1987@gmail.com>
2017-02-03 01:41:40 +0100

    Changed FSripPoint for AActor
    * trying to use AActors instread of FStripPoint

--- Source/AlphaOmega/ProceduralTubeStrip.cpp
+++ Source/AlphaOmega/ProceduralTubeStrip.cpp
@@ -7,10 +7,10 @@
 
 void AProceduralTubeStrip::GenerateMesh() {
 
-	if (stripPoints.Num() > 2) {
+	if (sstripPoints.Num() > 2) {
 		
-		FVector orientationBack = stripPoints[0].position - stripPoints[stripPoints.Num()-1].position;
-		FVector orientationForward = stripPoints[0].position - stripPoints[1].position;
+		FVector orientationBack = sstripPoints[0]->GetActorLocation() - sstripPoints[sstripPoints.Num()-1]->GetActorLocation();
+		FVector orientationForward = sstripPoints[0]->GetActorLocation() - sstripPoints[1]->GetActorLocation();
 		FVector rotation0 = orientationBack.GetSafeNormal();
 		FVector rotationEnd = rotation0;
 		FVector rotationStart;
@@ -22,17 +22,17 @@ void AProceduralTubeStrip::GenerateMesh() {
 		bool startCap;
 		bool endCap;
 
-		for (int i = 1; i < stripPoints.Num(); ++i) {
+		for (int i = 1; i < sstripPoints.Num(); ++i) {
 
 			startCap = i == 1 && addCaps;
-			endCap = i == stripPoints.Num() - 1 && addCaps;
+			endCap = i == sstripPoints.Num() - 1 && addCaps;
 
-			int nextIndex = i < stripPoints.Num() - 1 ? i + 1 : 0;
+			int nextIndex = i < sstripPoints.Num() - 1 ? i + 1 : 0;
 
 			rotationStart = rotationEnd;
 
-			orientationBack = stripPoints[i].position - stripPoints[i-1].position;
-			orientationForward = stripPoints[i].position - stripPoints[nextIndex].position;
+			orientationBack = sstripPoints[i]->GetActorLocation() - sstripPoints[i-1]->GetActorLocation();
+			orientationForward = sstripPoints[i]->GetActorLocation() - sstripPoints[nextIndex]->GetActorLocation();
 
 			rotationEnd = (orientationBack.GetSafeNormal() - orientationForward.GetSafeNormal()).GetSafeNormal();
 
@@ -41,13 +41,13 @@ void AProceduralTubeStrip::GenerateMesh() {
 			if (nextIndex == 0 && !isClosedStrip)
 				rotationEnd = orientationBack.GetSafeNormal();
 
-			BuildTube(stripPoints[i-1].position, stripPoints[i].position, rotationStart, rotationEnd, stripPoints[i-1].radius, stripPoints[i].radius, circleSections, smoothNormals, useUniqueTexture, startCap, endCap);
+			BuildTube(sstripPoints[i-1]->GetActorLocation(), sstripPoints[i]->GetActorLocation(), rotationStart, rotationEnd, sstripPoints[i-1]->GetActorScale().X, sstripPoints[i]->GetActorScale().X, circleSections, smoothNormals, useUniqueTexture, startCap, endCap);
 		
 		}
 
 		if (isClosedStrip) {
 
-			BuildTube(stripPoints[stripPoints.Num() - 1].position, stripPoints[0].position, rotationEnd, rotation0, stripPoints[stripPoints.Num() - 1].radius, stripPoints[0].radius, circleSections, smoothNormals, useUniqueTexture, false, false);
+			BuildTube(sstripPoints[sstripPoints.Num() - 1]->GetActorLocation(), sstripPoints[0]->GetActorLocation(), rotationEnd, rotation0, sstripPoints[sstripPoints.Num() - 1]->GetActorScale().X, sstripPoints[0]->GetActorScale().X, circleSections, smoothNormals, useUniqueTexture, false, false);
 		}
 		
 	}
